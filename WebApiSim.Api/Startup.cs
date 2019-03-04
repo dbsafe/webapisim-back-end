fdceed8da1fd579b62051dfed67fbbb54fb1899f
Ernesto Valcarcel <valcarcel_perez@hotmail.com>
2019-03-03 21:36:01 -0600

    Added swagger ui. Configured Json serializer.

--- WebApiSim.Api/Startup.cs
+++ WebApiSim.Api/Startup.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace WebApiSim.Api
 {
@@ -11,21 +14,40 @@ namespace WebApiSim.Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
-                //.AddJsonOptions(o =>
+            services.AddMvc()
+            .AddJsonOptions(o =>
+            {
+                //if (o.SerializerSettings.ContractResolver != null)
                 //{
-                //    if (o.SerializerSettings.ContractResolver != null)
-                //    {
-                //        var castedResolver = o.SerializerSettings.ContractResolver as Newtonsoft.Json.Serialization.DefaultContractResolver;
-                //        castedResolver.NamingStrategy = null; // manipulate json serializer setting to use uppercase for prop names.
-                //    }
-                //});
+                //    var castedResolver = o.SerializerSettings.ContractResolver as Newtonsoft.Json.Serialization.DefaultContractResolver;
+                //    castedResolver.NamingStrategy = null; // manipulate json serializer setting to use uppercase for prop names.
+                //}
+
+                o.SerializerSettings.Converters.Add(new StringEnumConverter());
+                o.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+            });
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "WebApiSim", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApiSim V1");
+                c.RoutePrefix = string.Empty;
+            });
 
             if (env.IsDevelopment())
             {
